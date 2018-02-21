2eca56ae15a7f16c372b8de2c410c71a8ea48161
darkclif <morpheoosmatrix@gmail.com>
2018-02-21 10:35:55 +0100

    ejercicio cine

--- app/src/main/java/com/example/ejerciciofinalcine/ResumenActivity.java
+++ app/src/main/java/com/example/ejerciciofinalcine/ResumenActivity.java
@@ -2,11 +2,13 @@ package com.example.ejerciciofinalcine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ResumenActivity extends AppCompatActivity {
 
     TextView txt_nombre,txt_año,txt_genero,txt_temporadas,txt_episodios;
+    ImageView img_genero;
 
 
 
@@ -20,16 +22,25 @@ public class ResumenActivity extends AppCompatActivity {
         txt_genero=(TextView)findViewById(R.id.resu_txtgenero);
         txt_temporadas=(TextView)findViewById(R.id.resu_txttemporada);
         txt_episodios=(TextView)findViewById(R.id.resu_txtepisodio);
+        img_genero=(ImageView)findViewById(R.id.resu_img);
 
         Bundle bundlist = getIntent().getExtras();
         if (bundlist!=null){
 
             Serie seriado = bundlist.getParcelable(ListViewActivity.EXTRA_SERIE);
+
+            String foto=seriado.getImagen();
             txt_nombre.setText(seriado.getNombre());
             txt_año.setText(seriado.getAño()+"");
             txt_genero.setText(seriado.getGenero_superior());
             txt_temporadas.setText(seriado.getTemporadas()+"");
             txt_episodios.setText(seriado.getEpisodios()+"");
+
+            int idfoto = getApplicationContext().getResources().getIdentifier(foto,"drawable",getApplicationContext().getPackageName());
+
+
+
+            img_genero.setImageResource(idfoto);
         }
     }
 }
